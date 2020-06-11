0dd873f7ec201c6ccc26242ca22a3b53de5c9e1e
Damian Perera <damianperera@outlook.com>
2020-06-11 17:40:54 +0530

    chore: added versioning for test deps

--- test/deps.ts
+++ test/deps.ts
@@ -1,3 +1,3 @@
-export { stub, resolves } from 'https://deno.land/x/mock/stub.ts'
-export { assertEquals, assert } from 'https://deno.land/std/testing/asserts.ts'
+export { stub, resolves } from 'https://deno.land/x/mock@master/stub.ts'
+export { assertEquals, assert } from 'https://deno.land/std@master/testing/asserts.ts'
 export { ServerRequest } from 'https://deno.land/x/oak@master/deps.ts'
